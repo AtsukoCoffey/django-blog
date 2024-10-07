0420e2a4e03a39ee1d52dce66cb4e449d0f0d8c0
Atsuko Coffey <atsucoffey@gmail.com>
2024-10-07 21:53:06 +0000

    Add a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-atsukocoffey-djangoblog-b840vjqigvl.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-atsukocoffey-djangoblog-6uizehywsen.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
