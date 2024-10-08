449938c2dc446abea27234a0a9fa9f8449f91668
Atsuko Coffey <atsucoffey@gmail.com>
2024-10-08 21:33:08 +0000

    feat: Create blog urls.py and change views and ../urls

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_view
+
 
 urlpatterns = [
-    path('', index_view.index, name='index'),
+    path('', include('blog.urls'), name='blog-urls'),
     path('admin/', admin.site.urls),
 ]
