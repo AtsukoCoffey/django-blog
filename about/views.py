10e890c7d22ff0d7db53877ea1c7aa1374c45140
Atsuko Coffey <atsucoffey@gmail.com>
2024-10-17 11:34:25 +0000

    Handle the form POST request

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -8,6 +9,18 @@ def about_me(request):
     Renders the About Page
     """
     about = About.objects.all().order_by('-created_on').first()
+    collaborate_form = CollaborateForm()
+
+    # Handle the form POST request
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+    # Reset the form
     collaborate_form = CollaborateForm()
 
     return render(
